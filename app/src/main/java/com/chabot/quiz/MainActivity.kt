895609b7d95ebb9166d96d65b30766f5c4b63a1a
nickchabot <nicholaschabot14@stu.spusd.net>
2021-10-13 10:58:59 -0700

    need 2 finish

--- app/src/main/java/com/chabot/quiz/MainActivity.kt
+++ app/src/main/java/com/chabot/quiz/MainActivity.kt
@@ -13,6 +13,9 @@ lateinit var questionText : TextView
 lateinit var answerX : Button
 lateinit var answerY : Button
 lateinit var answerZ : Button
+lateinit var quiz: Quiz
+lateinit var currentquestion: Questions
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         wirewidgets()
@@ -30,13 +33,40 @@ class MainActivity : AppCompatActivity() {
         val type = object : TypeToken<List<Questions>>() { }.type
         val question = gson.fromJson<List<Questions>>(jsonText, type)
         Log.d(TAG, "onCreate: \n${question.toString()}")
+        answerX.setOnClickListener { quiz.score(0)
+        nextQuestion()
+        }
+        answerY.setOnClickListener { quiz.score(1)
+        nextQuestion()
+        }
+        answerZ.setOnClickListener { quiz.score(2)
+        nextQuestion()
+        }
+        questiontext()
+    }
+
+    fun nextQuestion(){
+        if(quiz.addquestions()){
+            currentquestion = quiz.getnextQuestion()
+        }
+        else{
+            questionText.text = quiz.score.toString()
+        }
+    }
+
+    fun questiontext(){
+        questionText.text = currentquestion.t
+        answerX.text = currentquestion.a1
+        answerY.text = currentquestion.a2
+        answerZ.text = currentquestion.a3
     }
 
     private fun wirewidgets()                                            {
         questionText = findViewById(R.id.questionText)
         answerX = findViewById(R.id.answerX)
         answerY = findViewById(R.id.answerY)
-        answerZ = findViewById(R.id.answerZ                                                                                                                                                                                                                                                                                                                                                                                                                                                   )
+        answerZ = findViewById(R.id.answerZ)
+
     }
 
 
--- app/src/main/java/com/chabot/quiz/Quiz.kt
+++ app/src/main/java/com/chabot/quiz/Quiz.kt
@@ -1,7 +1,20 @@
 package com.chabot.quiz
 
 class Quiz(var Questions: List<Questions>) {
-    fun checkAnswer() {
+    var score = 0
+    var index = 0
 
+    fun addquestions(): Boolean {
+        return index <= Questions.size
+    }
+
+    fun score(x:Int){
+        if(x == Questions[index].correct)
+            score++
+    }
+
+    fun getnextQuestion() : Questions{
+        index++
+        return Questions[index]
     }
 }
